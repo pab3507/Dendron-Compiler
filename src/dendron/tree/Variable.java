b037f014badb77188e4c68979f10e3ee48ba525d
Pord123 <pedroantbretonca@hotmail.com>
2018-02-16 17:17:49 -0500

    Implement Variable class

--- src/dendron/tree/Variable.java
+++ src/dendron/tree/Variable.java
@@ -1,4 +1,31 @@
 package dendron.tree;
 
-public class Variable {
+import dendron.machine.Machine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Variable implements ExpressionNode{
+    private String name;
+    public Variable(String name){
+        this.name = name;
+    }
+    @Override
+    public int evaluate(Map<String, Integer> symTab) {
+        return symTab.get(this.name);
+    }
+
+    @Override
+    public void infixDisplay() {
+        System.out.printf("%s",this.name);
+
+    }
+
+    @Override
+    public List<Machine.Instruction> emit() {
+        ArrayList<Machine.Instruction> list = new ArrayList<>();
+        list.add(new Machine.Load(this.name));
+        return list;
+    }
 }
