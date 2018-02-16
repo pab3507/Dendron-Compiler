6470792aae87fe380b5e124a7db6e4ce3a12be9c
Pord123 <pedroantbretonca@hotmail.com>
2018-02-16 17:45:43 -0500

    Implement Constant class

--- src/dendron/tree/Constant.java
+++ src/dendron/tree/Constant.java
@@ -1,4 +1,33 @@
 package dendron.tree;
 
-public class Constant {
+import dendron.machine.Machine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Constant implements ExpressionNode {
+    private final int value;
+
+    public Constant(int value){
+        this.value = value;
+    }
+
+    @Override
+    public int evaluate(Map<String, Integer> symTab) {
+        return this.value;
+    }
+
+    @Override
+    public void infixDisplay() {
+        System.out.printf("%d",this.value);
+
+    }
+
+    @Override
+    public List<Machine.Instruction> emit() {
+        ArrayList<Machine.Instruction> list = new ArrayList<>();
+        list.add(new Machine.PushConst(this.value));
+        return list;
+    }
 }
