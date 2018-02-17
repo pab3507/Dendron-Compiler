69bd1f7178ba2292bf75b3abe6272c1e60309496
Pord123 <pedroantbretonca@hotmail.com>
2018-02-16 20:05:51 -0500

    Implement Assignment Class

--- src/dendron/tree/Assignment.java
+++ src/dendron/tree/Assignment.java
@@ -1,4 +1,36 @@
 package dendron.tree;
 
-public class Assignment {
+import dendron.machine.Machine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Assignment implements ActionNode{
+    private String name;
+    private ExpressionNode rhs;
+
+    public Assignment(String ident, ExpressionNode rhs){
+        this.name = ident;
+        this.rhs = rhs;
+    }
+
+    @Override
+    public void execute(Map<String, Integer> symTab) {
+        symTab.put(this.name,this.rhs.evaluate(symTab));
+
+    }
+
+    @Override
+    public void infixDisplay() {
+        System.out.printf(":= %s ",this.name);
+        this.rhs.infixDisplay();
+    }
+
+    @Override
+    public List<Machine.Instruction> emit() {
+        ArrayList<Machine.Instruction> list = new ArrayList<>();
+        list.add(new Machine.Store(this.name));
+        return list;
+    }
 }
