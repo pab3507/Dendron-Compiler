2900d33b58168f05d3c0d55eb34a159af21cd5b9
Pord123 <pedroantbretonca@hotmail.com>
2018-02-20 17:12:27 -0500

    Implement BinaryOperation class

--- src/dendron/tree/BinaryOperation.java
+++ src/dendron/tree/BinaryOperation.java
@@ -1,4 +1,78 @@
 package dendron.tree;
 
-public class BinaryOperation {
+import dendron.Errors;
+import dendron.machine.Machine;
+
+import java.util.*;
+
+public class BinaryOperation implements ExpressionNode {
+    public static final String ADD = "+";
+    public static final String SUB = "-";
+    public static final String MUL = "*";
+    public static final String DIV = "/";
+    public static final Collection<String> OPERATORS = new ArrayList<>(Arrays.asList(ADD, SUB, MUL, DIV));
+
+    private String operator;
+    private ExpressionNode leftChild;
+    private ExpressionNode rightChild;
+
+    public BinaryOperation(String operator, ExpressionNode leftChild, ExpressionNode rightChild){
+        if (OPERATORS.contains(operator)){
+            this.operator = operator;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        } else{
+            Errors.report(Errors.Type.ILLEGAL_VALUE,String.format("%s is not a valid operator",operator));
+        }
+
+    }
+
+    @Override
+    public int evaluate(Map<String, Integer> symTab) {
+        int a =leftChild.evaluate(symTab);
+        int b = rightChild.evaluate(symTab);
+        switch (this.operator){
+            case ADD:
+                return a + b;
+            case SUB:
+                return a - b;
+            case MUL:
+                return a * b;
+            case DIV:
+                if (b!= 0) {
+                    return a / b;
+                } else {
+                    Errors.report(Errors.Type.DIVIDE_BY_ZERO,"Integer divide by zero");
+                }
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public void infixDisplay() {
+        System.out.printf("( %s %s %s )", this.leftChild, this.operator, this.rightChild);
+    }
+
+    @Override
+    public List<Machine.Instruction> emit() {
+        ArrayList<Machine.Instruction> list = new ArrayList<>();
+        list.addAll(this.leftChild.emit());
+        list.addAll(this.rightChild.emit());
+        switch (this.operator){
+            case ADD:
+                list.add(new Machine.Add());
+                break;
+            case SUB:
+                list.add(new Machine.Subtract());
+                break;
+            case MUL:
+                list.add(new Machine.Multiply());
+                break;
+            case DIV:
+                list.add(new Machine.Divide());
+                break;
+        }
+        return list;
+    }
 }
