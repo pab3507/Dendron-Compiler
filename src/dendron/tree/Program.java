8fb111575a8cd0b6e13d87d3b51cc38dae47a180
Pord123 <pedroantbretonca@hotmail.com>
2018-02-20 17:36:18 -0500

    Implement program class

--- src/dendron/tree/Program.java
+++ src/dendron/tree/Program.java
@@ -1,4 +1,44 @@
 package dendron.tree;
 
-public class Program {
+import dendron.machine.Machine;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Program implements ActionNode{
+    private LinkedList<ActionNode> actionList;
+
+    public Program(){
+        actionList = new LinkedList<>();
+    }
+
+    public void addAction(ActionNode newNode){
+        actionList.add(newNode);
+    }
+
+
+    @Override
+    public void execute(Map<String, Integer> symTab) {
+        for (ActionNode node : actionList){
+            node.execute(symTab);
+        }
+    }
+
+    @Override
+    public void infixDisplay() {
+        for (ActionNode node : actionList){
+            node.infixDisplay();
+        }
+    }
+
+    @Override
+    public List<Machine.Instruction> emit() {
+        ArrayList<Machine.Instruction> list = new ArrayList<>();
+        for (ActionNode node : actionList){
+            list.addAll(node.emit());
+        }
+        return list;
+    }
 }
